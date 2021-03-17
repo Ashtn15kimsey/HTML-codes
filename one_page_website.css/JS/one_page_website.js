2c83fd07e30196081ac45fcef593b77672c2191d
ashtn15kimsey <ashtn15@gmail.com>
2021-03-17 14:13:33 -0700

    update

--- one_page_website.css/JS/one_page_website.js
+++ one_page_website.css/JS/one_page_website.js
@@ -24,9 +24,9 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("class");
-  var dots = document.getElementsByClassName("class");
-  var captionText = document.getElementById("value");
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -35,7 +35,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "class";
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
